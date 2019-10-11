83d0f51109abf679a16b6cd94d254659a60ce505
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2019-10-10 22:16:33 -0700

    styling

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className='Tilt' options={{ max : 55 }} style={{ height: 150, width: 150 }}>
+        <div className='Tilt-inner pa3'>
+          <img src={brain} style={{paddingTop: '5px'}} alt='brain png'/>
+        </div>
+      </Tilt>
+    </div>
+  );
+}
+
+export default Logo;
