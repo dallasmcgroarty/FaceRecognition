9e771e080fdb27f591a122474199743af9a94943
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2019-10-28 19:32:50 -0700

    Updates

---
+++ precache-manifest.6091e94dc97df032c4d01686f77a34f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ca44846d194b9c66270c39b430c12b",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "412549e61a119a40bb7d",
+    "url": "/FaceRecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e3c3aee878a8e61e6edb",
+    "url": "/FaceRecognition/static/css/main.4030dd77.chunk.css"
+  },
+  {
+    "revision": "412549e61a119a40bb7d",
+    "url": "/FaceRecognition/static/js/2.2323d94f.chunk.js"
+  },
+  {
+    "revision": "e3c3aee878a8e61e6edb",
+    "url": "/FaceRecognition/static/js/main.68c53e1e.chunk.js"
+  },
+  {
+    "revision": "e892295f5db7258b01b0",
+    "url": "/FaceRecognition/static/js/runtime-main.1e449fcb.js"
+  }
+]);
