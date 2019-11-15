f63095f8a75967fa771670884f0b4e6f7667684e
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2019-11-15 00:21:08 -0800

    Updates

---
+++ precache-manifest.30e12ac20c6bca259e8eae58240b396f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db68e835a6eea0fb7eeb175107e786ef",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "2492c0128be05e8bfd68",
+    "url": "/FaceRecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e464ea45784943bd323a",
+    "url": "/FaceRecognition/static/css/main.e1c93274.chunk.css"
+  },
+  {
+    "revision": "2492c0128be05e8bfd68",
+    "url": "/FaceRecognition/static/js/2.d433e92b.chunk.js"
+  },
+  {
+    "revision": "e464ea45784943bd323a",
+    "url": "/FaceRecognition/static/js/main.d60de520.chunk.js"
+  },
+  {
+    "revision": "e892295f5db7258b01b0",
+    "url": "/FaceRecognition/static/js/runtime-main.1e449fcb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FaceRecognition/precache-manifest.6091e94dc97df032c4d01686f77a34f5.js"
+  "/FaceRecognition/precache-manifest.30e12ac20c6bca259e8eae58240b396f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FaceRecognition/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
